3075397da75ed6314505c1f77044812661d57dfe
meti2626 <metseboka@gmail.com>
2026-03-28 04:32:59 +0300

    3/28/2026

---
+++ resources/views/home.blade.php
@@ -0,0 +1,15 @@
+
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <title>Document</title>
+</head>
+<body>
+      <h1> Testing 123</h1>
+</body>
+</html>
+
+
+

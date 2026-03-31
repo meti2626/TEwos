1d56850901a946c8dfdf5a7427dc9af46d4c5853
meti2626 <metseboka@gmail.com>
2026-03-31 16:22:23 +0300

    what is this

--- react/src/views/Signup.jsx
+++ react/src/views/Signup.jsx
@@ -1,10 +1,34 @@
-import { Link } from 'react-router-dom';
+import { Link  } from 'react-router-dom';
+import {useRef} from "react";
+import axiosClient from '../axios-client.js';
+import {useStateContext}  from "../context/ContextProvider.jsx";
 
 export default function Signup() {
+
+  const nameRef = useRef();
+  const emailRef = useRef();
+    const passwordRef = useRef();
+    const confirmPasswordRef = useRef();
+
+   const {setUser,setToken} = useStateContext()
+
 
   const onSubmit = (ev) =>
   {
     ev.preventDefault()
+   const payload ={
+    name: nameRef.current.value,
+    email: emailRef.current.value,
+    password: passwordRef.current.value,
+    confirmPassword: confirmPasswordRef.current.value
+   }
+
+
+     axiosClient.post('/signup', payload)
+        .then(({data}) =>{
+            setUser(data.user)
+            setToken(data.token)
+        })
   }
 
   return (
@@ -13,10 +37,10 @@ export default function Signup() {
                 <h1 className='title'>
                     Signup
                 </h1>
-                <input type='text' placeholder='Full Name'   />
-                <input type='email' placeholder='Email'   />
-                <input type='password'  placeholder='Password' />
-                <input type='password'  placeholder='Confirm Password' />
+                <input type='name' placeholder='Full Name' ref={nameRef} />
+                <input type='email' placeholder='Email' ref={emailRef} />
+                <input type='password'  placeholder='Password' ref={passwordRef} />
+                <input type='password'  placeholder='Confirm Password' ref={confirmPasswordRef} />
                 <button className='btn btn-block'>Signup</button>
 
                 <p className='message'>
